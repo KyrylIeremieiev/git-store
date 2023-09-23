5924431d4ec30ff8ef33a594cad5cae0e396a7ed
KyrylIeremieiev <34677@ma-web.nl>
2023-09-23 17:54:36 +0200

    change to github styling

--- web/src/js/login.js
+++ web/src/js/login.js
@@ -3,8 +3,9 @@ class Login{
         this.name = name;
         this.pass = pass;
         this.btn = btn;
-        console.log(btn)
         this.btn.onclick = this.getData;
+        this.cookie = this.getCookie('user')
+        console.log(this.cookie)
     }
 
     getData = () =>{
@@ -15,7 +16,7 @@ class Login{
                 'Content-Type': 'application/json'
                 },
                 method: "POST",
-                body: JSON.stringify({"username": "bogus", "pass": "bogus"})
+                body: JSON.stringify({"username": this.name.value, "pass": this.pass.value})
             })
             .then(response => {
                 if (response.ok) {
@@ -27,8 +28,28 @@ class Login{
             .then(data => {
                 // Handle the response data (query results) as needed
                 console.log(data);
+                this.data = data
+                document.cookie = "user=" + data[0].cookie;
             })
+    }
+
+    getCookie = (name) =>{
+        // Split all cookies into an array
+        const cookies = document.cookie.split(';');
+    
+        // Loop through the cookies to find the one with the specified name
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim(); // Remove leading/trailing spaces
+            if (cookie.startsWith(name + '=')) {
+                // If the cookie starts with the specified name
+                // Extract and return the cookie value
+                return decodeURIComponent(cookie.substring(name.length + 1));
+            }
+        }
+    
+        // If the cookie with the specified name is not found, return null
+        return null;
     }
 }
 
-let signup = new Login(document.getElementById('username'), document.getElementById('pass'), document.getElementById('submit'))
+let login = new Login(document.getElementById('username'), document.getElementById('pass'), document.getElementById('submit'))
